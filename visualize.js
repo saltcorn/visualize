583747d9ddb3b820959dfe6ffd4d7f3c6d5be7b8
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2021-09-09 16:22:41 +0000

    update address

--- visualize.js
+++ visualize.js
@@ -2,8 +2,7 @@ module.exports = {
   headers: [
     {
       script:
-        "https://cdnjs.cloudflare.com/ajax/libs/plotly.js/1.54.5/plotly.min.js",
-      integrity: "sha256-qXgZ3jy1txdNZG0Lv20X3u5yh4892KqFcfF1SaOW0gI=",
+        "/plugins/public/visualize/plotly.min.js"
     },
   ],
   sc_plugin_api_version: 1,
