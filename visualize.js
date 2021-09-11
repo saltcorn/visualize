d02fe666e6a53b7df9442e97fa24c3cc2d086709
Tom Nielsen <tanielsen@gmail.com>
2021-09-11 09:59:06 +0100

    plugin name, version

--- visualize.js
+++ visualize.js
@@ -1,11 +1,11 @@
 module.exports = {
   headers: [
     {
-      script:
-        "/plugins/public/visualize/plotly.min.js"
+      script: "/plugins/public/visualize/plotly.min.js",
     },
   ],
   sc_plugin_api_version: 1,
+  plugin_name: "visualize",
   viewtemplates: [
     require("./proportions"),
     require("./scatter"),
