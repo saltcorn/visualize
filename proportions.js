57400f1ab426b9636adbfb9f09bc63cf2f542b05
Christian Hugo <hugo.christian@web.de>
2024-04-28 15:00:15 +0200

    comma separated color configuration

--- proportions.js
+++ proportions.js
@@ -17,17 +17,17 @@ const configuration_workflow = () =>
     ],
   }); //http://localhost:3000/view/PIES?category=Fun
 
-const run = async (table_id, viewname, cfg, state, extraArgs) => {
-  const table = await Table.findOne(table_id);
-  return await proportionsPlot(table, cfg, state, extraArgs.req);
-};
-
-module.exports = {
-  name: "ProportionsVis",
-  description:
-    "Visualise proportions in a table with a bar, pie or doughnut chart",
-  display_state_form: false,
-  get_state_fields,
-  configuration_workflow,
-  run,
+module.exports = (context) => {
+  return {
+    name: "ProportionsVis",
+    description:
+      "Visualise proportions in a table with a bar, pie or doughnut chart",
+    display_state_form: false,
+    get_state_fields,
+    configuration_workflow,
+    run: async (table_id, viewname, cfg, state, extraArgs) => {
+      const table = await Table.findOne(table_id);
+      return await proportionsPlot(table, cfg, state, extraArgs.req, context);
+    },
+  };
 };
--- visualize.js
+++ visualize.js
@@ -1,19 +1,54 @@
 const { features } = require("@saltcorn/data/db/state");
+const Form = require("@saltcorn/data/models/form");
+const Workflow = require("@saltcorn/data/models/workflow");
 
 module.exports = {
-  headers: [
+  headers: (cfg) => [
     {
-      script: `/plugins/public/visualize${features?.version_plugin_serve_path
-        ? "@" + require("./package.json").version
-        : ""
-        }/plotly.min.js`,
+      script: `/plugins/public/visualize${
+        features?.version_plugin_serve_path
+          ? "@" + require("./package.json").version
+          : ""
+      }/plotly.min.js`,
     },
   ],
   sc_plugin_api_version: 1,
   plugin_name: "visualize",
-  viewtemplates: [
-    require("./proportions"),
-    require("./scatter"),
-    require("./data-explorer"),
-  ],
+  viewtemplates: (cfg) => {
+    return [
+      require("./proportions")(cfg),
+      require("./scatter"),
+      require("./data-explorer"),
+    ];
+  },
+  configuration_workflow: () => {
+    return new Workflow({
+      steps: [
+        {
+          name: "colors",
+          form: () => {
+            return new Form({
+              fields: [
+                {
+                  name: "colors",
+                  label: "Colors",
+                  type: "String",
+                  sublabel:
+                    "Comma-separated list of colors for ProportionsVis views (in Hex-format e.g. #2c3e50,#95a5a6,#18bc9c)",
+                  validator: (s) => {
+                    if (s) {
+                      const colors = s.split(",");
+                      if (colors.some((c) => !c.match(/^#[0-9a-fA-F]{6}$/)))
+                        return "Invalid color format";
+                    }
+                    return true;
+                  },
+                },
+              ],
+            });
+          },
+        },
+      ],
+    });
+  },
 };
