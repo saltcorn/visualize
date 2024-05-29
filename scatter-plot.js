60a3871976d0f8e020c0a2478564c8242773013f
Tom Nielsen <tanielsen@gmail.com>
2024-05-29 18:26:06 +0100

    multiple relation plots

--- scatter-plot.js
+++ scatter-plot.js
@@ -1,4 +1,5 @@
 const Field = require("@saltcorn/data/models/field");
+const FieldRepeat = require("@saltcorn/data/models/fieldrepeat");
 const Table = require("@saltcorn/data/models/table");
 const Form = require("@saltcorn/data/models/form");
 const db = require("@saltcorn/data/db");
@@ -24,33 +25,6 @@ const scatterForm = async (table, autosave) => {
   return new Form({
     fields: [
       {
-        name: "x_field",
-        label: "X axis field",
-        type: "String",
-        required: true,
-        attributes: {
-          options: maybeAddDisabledTitle(x_fields),
-        },
-      },
-      {
-        name: "y_field",
-        label: "Y axis field",
-        type: "String",
-        required: true,
-        attributes: {
-          options: maybeAddDisabledTitle(y_fields),
-        },
-      },
-      {
-        name: "style",
-        label: "Style",
-        type: "String",
-        required: true,
-        attributes: {
-          options: maybeAddDisabledTitle(["lines", "markers", "lines+markers"]),
-        },
-      },
-      {
         name: "title",
         label: "Plot title",
         type: "String",
@@ -63,6 +37,85 @@ const scatterForm = async (table, autosave) => {
         required: true,
         default: 450,
       },
+      {
+        name: "x_field",
+        label: "X axis field",
+        type: "String",
+        required: true,
+        attributes: {
+          options: maybeAddDisabledTitle(x_fields),
+        },
+      },
+      {
+        name: "num_plots",
+        label: "Plot series",
+        type: "String",
+        required: true,
+        attributes: {
+          options: ["Single", "Multiple"],
+        },
+      },
+      {
+        name: "y_field",
+        label: "Y axis field",
+        type: "String",
+        required: true,
+        attributes: {
+          options: maybeAddDisabledTitle(y_fields),
+        },
+        showIf: { num_plots: "Single" },
+      },
+      {
+        name: "style",
+        label: "Style",
+        type: "String",
+        required: true,
+        attributes: {
+          options: maybeAddDisabledTitle(["lines", "markers", "lines+markers"]),
+        },
+        showIf: { num_plots: "Single" },
+      },
+      {
+        name: "y_title",
+        label: "Y axis title",
+        type: "String",
+        showIf: { num_plots: "Multiple" },
+        required: false,
+      },
+      new FieldRepeat({
+        name: "series",
+        label: "Series",
+        showIf: { num_plots: "Multiple" },
+        fields: [
+          {
+            name: "y_field",
+            label: "Y axis field",
+            type: "String",
+            required: true,
+            attributes: {
+              options: maybeAddDisabledTitle(y_fields),
+            },
+          },
+          {
+            name: "style",
+            label: "Style",
+            type: "String",
+            required: true,
+            attributes: {
+              options: maybeAddDisabledTitle([
+                "lines",
+                "markers",
+                "lines+markers",
+              ]),
+            },
+          },
+          {
+            name: "color",
+            label: "Color",
+            type: "Color",
+          },
+        ],
+      }),
     ],
   });
 };
@@ -79,7 +132,7 @@ const plotly = (id, ...args) =>
 
 const scatterPlot = async (
   table,
-  { x_field, y_field, style, title, height },
+  { x_field, y_field, style, title, y_title, height, num_plots, series },
   state
 ) => {
   const fields = await table.getFields();
@@ -89,27 +142,52 @@ const scatterPlot = async (
   const yfld = fields.find((f) => f.name === y_field);
   const where = await stateFieldsToWhere({ fields, state });
   const rows = await table.getRows(where, { orderBy: x_field });
-  const y = rows.map((r) => r[yfld.name]);
-  const x = rows.map((r) => r[xfld.name]);
-  const data = [
-    {
+  const data = [];
+  if (num_plots === "Multiple") {
+    for (const { y_field, style, color } of series) {
+      const y = rows.map((r) => r[y_field]);
+      const x = rows.map((r) => r[xfld.name]);
+      const pt = {
+        type: "scatter",
+        mode: style,
+        x,
+        y,
+      };
+      if (style === "lines") pt.line = { color };
+      if (style === "markers") pt.markers = { color };
+      if (style === "lines+markers") {
+        pt.line = { color };
+        pt.markers = { color };
+      }
+      data.push(pt);
+    }
+  } else {
+    const y = rows.map((r) => r[yfld.name]);
+    const x = rows.map((r) => r[xfld.name]);
+    const pt = {
       type: "scatter",
       mode: style,
       x,
       y,
-    },
-  ];
+    };
+
+    data.push(pt);
+  }
   var config = {
     displayModeBar: false,
     responsive: true,
   };
+  const ytitle =
+    num_plots === "Single"
+      ? fieldToLabel(yfld)
+      : y_title || series.map((s) => s.y_field).join(" ");
   var layout = {
     title,
     showlegend: false,
     height: +height,
     margin: title ? { pad: 4, t: 40, r: 25 } : { pad: 4, t: 10, r: 25 },
     xaxis: { title: fieldToLabel(xfld), automargin: true },
-    yaxis: { title: fieldToLabel(yfld), automargin: true },
+    yaxis: { title: ytitle, automargin: true },
   };
   return (
     div({ id: divid }) +
